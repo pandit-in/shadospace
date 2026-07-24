575d6f485c18bb8748b9139b29544fdf1d16eae2
pandit <pandit.inn@gmail.com>
2026-07-24 16:48:51 +0530

    removed footer from the layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/layout/header"
-import Footer from "@/components/layout/footer"
+// import Footer from "@/components/layout/footer"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import React from "react"
 
@@ -9,14 +9,14 @@ export default function MainLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <SidebarProvider>
         {/* <AppSidebar /> */}
         <div className="w-full flex-1">{children}</div>
         {/* <RightSidebar /> */}
       </SidebarProvider>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
